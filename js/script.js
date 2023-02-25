dca792f06f25a36998391871052eb827e84b7f82
mayaralvs <126295795+mayaralvs@users.noreply.github.com>
2023-02-24 22:08:32 -0300

    new

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const a = Number(prompt("Digite o Salário"))
 const b = Number(prompt("Digite o aumento"))
 
 
-resultado = a+(a*b/100)
+let resultado = a+(a*b/100)
 console.log(resultado)
 
 document.write(resultado)
